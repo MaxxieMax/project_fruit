3bf52ae7da9aaf3aadb0f6581722bba4348c5add
MaxxieMax <Max.breunesse@gmail.com>
2020-05-28 20:25:47 +0200

    basic shit

---
+++ nav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg " id="#">
+  <a class="navbar-brand" id="nav_text_1" href="#"> <img src="./images/yoda.jpg" width="80" height="80"
+      class="d-inline-block align-top" alt="">
+    </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <ul class="navbar-nav">
+      <a class="nav-item nav-link active" href="index.php?content=homepage" id="nav_text_1">Home<span
+          class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" id="nav_text_1" href="index.php?content=producten">Producten</a>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" id="nav_text_1" href="index.php?content=Registratie">Registratie</a>
+      <a class="nav-item nav-link" id="nav_text_1" href="index.php?content=login">Inloggen</a>
+    </ul>
+  </div>
+</nav>
+

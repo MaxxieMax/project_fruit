2c4d91f9a5ebc0fae618b9668f675eea647e0fca
MaxxieMax <Max.breunesse@gmail.com>
2020-06-18 10:29:52 +0200

    update het was niet goed gegaan

--- activate_script.php
+++ activate_script.php
@@ -4,8 +4,15 @@ include("./functions.php");
 
 $id = sanitize($_POST["id"]);
 $pwh = sanitize($_POST["pwh"]);
+$password = sanitize($_POST["password"]);
+$passwordCheck = sanitize($_POST["passwordCheck"]);
+
 
 if(empty($_POST["password"]) || empty($_POST["passwordCheck"])) {
     header("location: ./index.php?content=message&alert=password-empty&id=$id&pwh=$pwh");
+} elseif (strcmp($password, $passwordCheck)){
+    echo "niet gelijk";
+} else {
+    echo "gelijk";
 }
-?>
+?> 
